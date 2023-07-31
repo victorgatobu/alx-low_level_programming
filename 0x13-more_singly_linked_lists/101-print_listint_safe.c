53b8b64bef870c677cdf3fea48afab0f1103b15e
victorgatobu <gatobuvictor@gmail.com>
2023-07-31 08:43:52 -0700

    debug

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -1,36 +1,36 @@
 #include "lists.h"
-#include <stdio.h>
-
 /**
- * print_listint_safe - Print a `listint_t` linked list including mem addresses
- * @head: head of linked list
- * Description: Go through the list only once.
- * Return: number of nodes in list. If fails, exit with status 98.
+ * print_listint_safe - function that prints a linked list with a loop safely.
+ * @head: pointer to the 1st node of the linked list
+ * Return: new_node
  */
 size_t print_listint_safe(const listint_t *head)
 {
-	const listint_t *current;
-	size_t count;
-	const listint_t *hold;
+	const listint_t *tmp_n = NULL;
+	const listint_t *l_n = NULL;
+	size_t counter = 0;
+	size_t new_n;
 
-	current = head;
-	if (current == NULL)
-		exit(98);
-
-	count = 0;
-	while (current != NULL)
+	tmp_n = head;
+	while (tmp_n)
 	{
-		hold = current;
-		current = current->next;
-		count++;
-		printf("[%p] %d\n", (void *)hold, hold->n);
-
-		if (hold < current)
+		printf("[%p] %d\n", (void *)tmp_n, tmp_n->n);
+		counter++;
+		tmp_n = tmp_n->next;
+		l_n = head;
+		new_n = 0;
+		while (new_n < counter)
 		{
-			printf("-> [%p] %d\n", (void *)current, current->n);
-			break;
+			if (tmp_n == l_n)
+			{
+				printf("-> [%p] %d\n", (void *)tmp_n, tmp_n->n);
+				return (counter);
+			}
+			l_n = l_n->next;
+			new_n++;
 		}
+		if (!head)
+			exit(98);
 	}
-
-	return (count);
+	return (counter);
 }
--- 0x13-more_singly_linked_lists/103-find_loop.c
+++ 0x13-more_singly_linked_lists/103-find_loop.c
@@ -1,38 +1,34 @@
 #include "lists.h"
-#include <stdio.h>
+
 /**
- * find_listint_loop - find the loop in a linked list
- * @head: head of linked list
- * Description: Not allowed to use malloc, free or arrays.
- * Can only declare a max of 2 variables.
- * Return: Address of node where loop starts, or NULL if no loop found.
+ * find_listint_loop - finds the loop in a linked list
+ * @head: linked list to search for
+ *
+ * Return: address of the node where the loop starts, or NULL
  */
 listint_t *find_listint_loop(listint_t *head)
 {
-	listint_t *currents, *currentf;
+	listint_t *slow = head;
+	listint_t *fast = head;
 
-	if (head == NULL)
+	if (!head)
 		return (NULL);
 
-	currents = currentf = head;
-	do {
-		if (currents->next)
-			currents = currents->next;
-		else
-			return (NULL);
-
-		if (currentf->next->next)
-			currentf = currentf->next->next;
-		else
-			return (NULL);
-	} while (currentf != currents);
-
-	currents = head;
-	while (currentf != currents)
+	while (slow && fast && fast->next)
 	{
-		currentf = currentf->next;
-		currents = currents->next;
+		fast = fast->next->next;
+		slow = slow->next;
+		if (fast == slow)
+		{
+			slow = head;
+			while (slow != fast)
+			{
+				slow = slow->next;
+				fast = fast->next;
+			}
+			return (fast);
+		}
 	}
 
-	return (currents);
+	return (NULL);
 }
